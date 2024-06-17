542d720da8e5a3a6fd8519e60524eea9df8c5879
kizito02 <kizitooyedu15@gmail.com>
2024-06-17 14:20:15 +0100

    added header component

---
+++ src/app/component/header.tsx
@@ -0,0 +1,14 @@
+// import Link from 'next/link'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='h-[70px] w-full flex items-center border-b bg-blue-500'>
+        <div className='gap-4 flex'>
+            <div></div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
